bfd44a6efb0bf2469ca113832dd858b6b6d79a44
uriel levy <uriellevy4892@gmail.com>
2021-10-30 13:34:35 +0300

    hero slider added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+///////NAVBAR////////
+
 const open_btn = document.querySelector(".open-btn");
 const close_btn = document.querySelector(".close-btn");
 const nav = document.querySelectorAll(".nav");
@@ -15,3 +17,75 @@ close_btn.addEventListener("click", () => {
   navContainer.classList.remove("none");
   logoOpen.classList.remove("none");
 });
+
+/////////HERO SLIDER//////////
+
+///////SLIDER///////////
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // get current class
+  const current = document.querySelector(".current");
+  //remove current class
+  current.classList.remove(".current");
+
+  //check for next slide
+
+  if (current.nextElementSibling) {
+    //add current to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    //add current to start
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => {
+    current.classList.remove("current");
+  });
+};
+
+const prevSlide = () => {
+  // get current class
+  const current = document.querySelector(".current");
+  //remove current class
+  current.classList.remove(".current");
+
+  //check for next slide
+
+  if (current.previousElementSibling) {
+    //add current to next sibling
+    current.previousElementSibling.classList.add("current");
+  } else {
+    //add current to last
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => {
+    current.classList.remove("current");
+  });
+};
+
+//button events
+next.addEventListener("click", (e) => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+prev.addEventListener("click", (e) => {
+  prevSlide();
+  clearInterval(slideInterval);
+
+  slideInterval = setInterval(nextSlide, intervalTime);
+});
+
+///auto-slide
+if (auto) {
+  //run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
